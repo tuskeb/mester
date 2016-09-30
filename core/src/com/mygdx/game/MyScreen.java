dae0c01e1cacd6c875e58dc20694c60706903504
tuskeb <tuskebganz@gmail.com>
2016-09-30 14:39:29 +0200

    MyScreen

---
+++ core/src/com/mygdx/game/MyScreen.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by tuskeb on 2016. 09. 30..
+ */
+abstract public class MyScreen implements Screen {
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
